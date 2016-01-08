e2391d5265433488f4c0a8f090ea1b597f8f4a99
StefanT <shtefanntz@yahoo.com>
2016-01-08 07:49:30 +0200

    started working on the reddit viewer

---
+++ src/redux/modules/currencyViewer.js
@@ -0,0 +1,36 @@
+import {createAction, handleActions } from 'redux-actions'
+import fetch from 'isomorphic-fetch'
+
+export const REQUEST_POSTS = 'REQUEST_POSTS'
+export const RECEIVE_POSTS = 'RECEIVE_POSTS'
+
+export const requestRedditPosts = createAction(REQUEST_POSTS)
+export const receiveRedditPosts = createAction(RECEIVE_POSTS, (reddit, json) => ({
+                                                                  reddit,
+                                                                  posts: json.data.children.map(child => child.data),
+                                                                  receivedAt: Date.now()
+                                                              })
+
+export function fetchPosts(reddit) {
+  return dispatch => {
+    dispatch(requestRedditPosts(reddit))
+    return fetch(`http://www.reddit.com/r/${reddit}.json`)
+      .then(response => response.json())
+      .then(json => dispatch(receiveRedditPosts(reddit, json)))
+  }
+}
+
+export default handleActions({
+  [REQUEST_POSTS] : (state = {isFetching: false, items: []}) => {
+    return Object.assign({}, state, {
+      isFetching: true
+    })
+  },
+  [RECEIVE_POSTS]: (state = {isFetching: false, items: []}, {payload}) => {
+    return Object.assign({}, state,{
+      isFetching: false,
+      items: payload.items.posts,
+      lastUpdated: action.receivedAt
+    })
+  }
+})
